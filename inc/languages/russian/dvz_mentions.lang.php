57d9292e3f5c47c280ab2697c554359bdb42b131
Anton Pupkov <pupant@gmail.com>
2015-09-11 13:22:10 -0700

    Fixed invalid formatting

--- inc/languages/russian/dvz_mentions.lang.php
+++ inc/languages/russian/dvz_mentions.lang.php
@@ -2,7 +2,7 @@
 /* by Tomasz 'Devilshakerz' Mlynski [devilshakerz.com]; Copyright (C) 2014-2015
  released under Creative Commons BY-NC-SA 4.0 license: http://creativecommons.org/licenses/by-nc-sa/4.0/ */
 
-$l['dvz_me_description'] = Отмечает <i>@username</i> с ссылкой к профилю и цветом группы. Интегрировано с <i>MyAlerts 2.0.x</i>.'
+$l['dvz_me_description'] = 'Отмечает <i>@username</i> с ссылкой к профилю и цветом группы. Интегрировано с <i>MyAlerts 2.0.x</i>.'
 ;
 $l['dvz_me_alerts'] = '<br><br><b>MyAlerts:</b>';
 $l['dvz_me_alerts_install'] = 'установить';
