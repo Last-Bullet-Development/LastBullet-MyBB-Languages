416a351d945a61c5738bddacd716c27b836a2653
Ulysses <udevide@gmail.com>
2015-09-12 07:03:41 +1100

    Update dvz_mentions.lang.php

--- inc/languages/russian/dvz_mentions.lang.php
+++ inc/languages/russian/dvz_mentions.lang.php
@@ -3,8 +3,8 @@
  released under Creative Commons BY-NC-SA 4.0 license: http://creativecommons.org/licenses/by-nc-sa/4.0/ */
 
 $l['dvz_me_description'] = 'Parses <i>@username</i> into profile link with group color. Integrates with <i>MyAlerts 2.0.x</i>.';
-$l['dvz_me_alerts'] = '<br><br><b>MyAlerts integrations:</b>';
-$l['dvz_me_alerts_install'] = 'install';
-$l['dvz_me_alerts_uninstall'] = 'uninstall';
-$l['dvz_me_alerts_installed'] = 'MyAlerts integration has been installed.';
-$l['dvz_me_alerts_uninstalled'] = 'MyAlerts integration has been uninstalled.';
+$l['dvz_me_alerts'] = '<br><br><b>MyAlerts:</b>';
+$l['dvz_me_alerts_install'] = 'установить';
+$l['dvz_me_alerts_uninstall'] = 'удалить';
+$l['dvz_me_alerts_installed'] = 'Функция MyAlerts установлена.';
+$l['dvz_me_alerts_uninstalled'] = 'Функция MyAlerts удалена.';
